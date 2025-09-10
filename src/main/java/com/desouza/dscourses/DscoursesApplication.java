9e919b35ce3bfe59f091a645e16b48f4466878b2
ozysouza <ozysouzaa@gmail.com>
2025-09-09 23:41:08 -0400

    Create DSCourses project

---
+++ src/main/java/com/desouza/dscourses/DscoursesApplication.java
@@ -0,0 +1,13 @@
+package com.desouza.dscourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desouza/dscourses/DscoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desouza.dscourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
