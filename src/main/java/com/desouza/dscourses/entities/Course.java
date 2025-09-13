3dc008bf465b97fec0e50a80bc913f63b5fb226e
ozysouza <ozysouzaa@gmail.com>
2025-09-12 22:43:42 -0400

    Dev: Create, Map and add relationship between Offer and Course

--- src/main/java/com/desouza/dscourses/entities/Course.java
+++ src/main/java/com/desouza/dscourses/entities/Course.java
@@ -1,11 +1,14 @@
 package com.desouza.dscourses.entities;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +21,9 @@ public class Course {
     private String name;
     private String imgUri;
     private String imgGrayUri;
+
+    @OneToMany(mappedBy = "course")
+    private List<Offer> offers = new ArrayList<>();
 
     public Course() {
     }
@@ -59,6 +65,10 @@ public class Course {
 
     public void setImgGrayUri(String imgGrayUri) {
         this.imgGrayUri = imgGrayUri;
+    }
+
+    public List<Offer> getOffers() {
+        return offers;
     }
 
     @Override
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,3 +13,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course(name, img_uri, img_gray_uri) VALUES ('Java Project', 'https://live.staticflickr.com/787/40932459844_f13b5acc93_z.jpg', 'https://www.adaptiveus.com/hs-fs/hubfs/undefined-Mar-21-2024-06-40-46-4269-AM.png?width=512&height=512&name=undefined-Mar-21-2024-06-40-46-4269-AM.png')
+
+INSERT INTO tb_offer(edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2025-09-15T15:00:00Z', TIMESTAMP WITH TIME ZONE '2026-09-15T15:00:00Z', 1)
+INSERT INTO tb_offer(edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2025-11-15T15:00:00Z', TIMESTAMP WITH TIME ZONE '2026-11-15T15:00:00Z', 1)
